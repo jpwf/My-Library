d1c17d334693d5fbda36e7234b1fe4d9bd6876a4
jpwf <64947903+jpwf@users.noreply.github.com>
2023-10-11 21:16:39 +0000

    11/10 update

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -7,15 +7,15 @@ function Navbar(){
   
   return(
     <>
-      <nav className={'w-full flex text-base justify-start gap-[750x] items-center border-b-[1px] h-[55px] border-b-gray-300 bg-[#B1830D] bg-opacity-20'}>
-        <div className='flex justify-start items-center'>
-          <div className=' ml-9 relative justify-start items-center flex-col items-center'>
+      <nav className={'w-full flex    items-center border-b-[1px] h-[55px] border-b-gray-300 bg-[#B1830D] bg-opacity-20'}>
+        <div className='flex justify-start items-center ml-9'>
+          <div className='  relative justify-start items-center flex-col items-center'>
             <button onClick={() => setNavBar((navbar) => !navbar)} 
             className='items-center ' >
               <List size={32} color="#1f1f1f" className={`${navbar ? "rotate-90 duration-500 linear" : ""}`} />
             </button>
             {navbar && (
-              <div className={`{${navbar} ? "flex duration-500" : "hidden"} bg-[#BC9A42] bg-opacity-25 grid justify-self-center w-[64px] h-[124px]  leading-6 place-items-center font-extrabold flex-col absolute rounded-2xl border `}>
+              <div className={`{${navbar} ? " duration-500" : "hidden"} bg-[#BC9A42] bg-opacity-25 grid justify-self-center w-[64px] h-[124px]  leading-6 place-items-center font-extrabold flex-col absolute rounded-2xl border `}>
                 <a>Ficção</a>
                 <a>Teste</a>
                 <a>Teste</a>
@@ -23,7 +23,7 @@ function Navbar(){
             )}
           </div>
         </div>
-        <div className='font-bold text-[42px] leading-[38px] '>
+        <div className='font-bold justify-center text-[42px] leading-[38px] w-full  ml-9 '>
           <h1>My Library</h1>
         </div>
 
